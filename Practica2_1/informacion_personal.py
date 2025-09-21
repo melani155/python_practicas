a8d14c591628f7d2b05176006029087270d17be2
melani <mxlani1003@gmail.com>
2025-09-20 23:24:04 -0600

    subo proyecto completo sin subrepositorios

---
+++ Practica2_1/informacion_personal.py
@@ -0,0 +1,16 @@
+#aqui se almacena mi nombre con la variable de nombre 
+nombre = "Melani"
+#lo mismo solo cambia el nombre de la variable
+edad = 17
+#y por ultimo el saludo q se guardara en la variable saludo
+saludo = "Bienvenidos a mis datos con python"
+
+#va a imprimir el nombre que hayas guardado en la variable
+print(f"nombre: {nombre}")
+#va a imprimir la edad que hayas guardado en la variable
+print(f"edad: {edad}")
+#va a imprimir lo que hayas guardado en la variable
+print(f"saludo: {saludo}")
+#mostrara lo que tengas guardado en la variable nombre
+#print(nombre)
+
